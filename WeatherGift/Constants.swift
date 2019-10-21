45343a63125f228c0fa3eb1d83a12d39ffd064b6
Jess <jessica@czachor.com>
2019-10-21 12:41:51 -0400

    7.15 Completed

---
+++ WeatherGift/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  WeatherGift
+//
+//  Created by Jess on 10/21/19.
+//  Copyright © 2019 Jess. All rights reserved.
+//
+
+import Foundation
+
+let urlBase = "https://api.darksky.net/forecast/"
+let urlAPIKey = "4e83f9249e18cee98061d8a39962684b/"
