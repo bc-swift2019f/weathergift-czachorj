d5abb4a72cca6d79565d9a5e7446f09dd31c8055
Jess <jessica@czachor.com>
2019-10-27 22:18:32 -0400

    7.20 completed

---
+++ WeatherGift/DayWeatherCell.swift
@@ -0,0 +1,24 @@
+//
+//  DayWeatherCell.swift
+//  WeatherGift
+//
+//  Created by Jess on 10/27/19.
+//  Copyright © 2019 Jess. All rights reserved.
+//
+
+import UIKit
+
+class DayWeatherCell: UITableViewCell {
+    @IBOutlet weak var dayCellIcon: UIImageView!
+    @IBOutlet weak var dayCellWeekday: UILabel!
+    @IBOutlet weak var dayCellMaxTemp: UILabel!
+    @IBOutlet weak var dayCellMinTemp: UILabel!
+    @IBOutlet weak var dayCellSummary: UITextView!
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+
+}
